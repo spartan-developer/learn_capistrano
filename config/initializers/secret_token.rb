a6803810af6b0cfa8cdc466aabb701a2f4b36f0a
Nicholas Yianilos <spartandeveloper@gmail.com>
2012-06-18 16:06:59 -0400

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearnCapistrano::Application.config.secret_token = '5817fafcd23ccb364fb7167d9fb2077a933217b1efbfd5279ac91c92cae21db7b21429aecb9983fbdd2eb5867f9e56ae895e043c16dde43558f5b084a602d878'
